a362c2243b8d19c3f559c4990dc2f4bedab0d469
Caroline Ortiz <carolineortizmartins@gmail.com>
2024-01-13 18:16:50 -0300

    Lista de produtos

---
+++ src/components/lists/ProductList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row q-pa-xs">
+    <div class="col-lg-2 q-pa-xs" v-for="product of products" :key="product.id">
+      <q-card>
+        <img :src="product.photo" />
+
+        <q-card-section>
+          <div class="text-body2 text-uppercase">
+            {{ product.name }}
+          </div>
+        </q-card-section>
+
+        <q-card-section>
+          <div class="text-subtitle2 row justify-between items-center">
+            R$ {{ product.price }}
+            <q-btn color="primary" icon="add_shopping_cart" round flat />
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  props: {
+    products: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,17 +1,44 @@
 <template>
-  <q-page class="flex flex-center">
-    <ProductForm />
+  <q-page class="bg-pink-1">
+    <ProductList :products="products" />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import ProductForm from "components/forms/ProductForm.vue";
+import ProductList from "components/lists/ProductList.vue";
 
 export default defineComponent({
   name: "ProductPage",
   components: {
-    ProductForm,
+    ProductList,
+  },
+  setup() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: "Batom Ruby Rose",
+          price: 24,
+          photo:
+            "https://static.cdnlive.com.br/uploads/724/unidade/16926543858831_zoom.jpg",
+        },
+        {
+          id: 2,
+          name: "Batom Ruby Rose",
+          price: 24.5,
+          photo:
+            "https://static.cdnlive.com.br/uploads/724/unidade/16926543858831_zoom.jpg",
+        },
+        {
+          id: 3,
+          name: "Batom Ruby Rose",
+          price: 24.99,
+          photo:
+            "https://static.cdnlive.com.br/uploads/724/unidade/16926543858831_zoom.jpg",
+        },
+      ],
+    };
   },
 });
 </script>
