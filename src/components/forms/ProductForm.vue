311ae1ac93b6542ada0ea5055b697e7993a0a175
Caroline Ortiz <carolineortizmartins@gmail.com>
2024-01-13 16:34:17 -0300

    Initial project

---
+++ src/components/forms/ProductForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-input v-model="name" label="Nome" />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { ref } from "vue";
+
+export default defineComponent({
+  name: "ProductForm",
+  setup() {
+    return {
+      name: ref(""),
+    };
+  },
+});
+</script>
---
+++ src/pages/ProductPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <ProductForm />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import ProductForm from "components/forms/ProductForm.vue";
+
+export default defineComponent({
+  name: "ProductPage",
+  components: {
+    ProductForm,
+  },
+});
+</script>
